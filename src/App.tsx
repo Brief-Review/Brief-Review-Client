04ee98a604a169a1e22459cf11f161d919e79743
Gary HL <garyhl321@gmail.com>
2023-04-30 22:04:16 +0200

    change public favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 function App() {
-
   return (
     <>
-        <h1 className="text-black font-bold text-4xl ">
-          Hola mundo
-        </h1>
+      <h1 className="text-black font-bold text-4xl ">Hola mundo</h1>
     </>
   );
 }
