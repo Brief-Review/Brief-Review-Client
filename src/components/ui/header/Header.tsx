ee070c8d4e36cf08c8ecf6ad969b6f865b7b5614
Gary HL <garyhl321@gmail.com>
2023-05-08 11:57:10 +0200

    use icon button in Header

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,15 +1,16 @@
 import { AiOutlineBell } from "react-icons/ai";
+import IconButton from "../common/IconButton";
 
 function Header() {
   return (
-    <div className="flex w-full justify-between h-12 my-8">
+    <div className="flex w-full justify-between h-12">
       <div className="w-full  flex justify-between items-center">
         <h1 className=" hidden md:hidden lg:block dark:text-white">
           Brief Review
         </h1>
-        <button className="h-12 aspect-square shadow-md text-primary bg-white flex items-center justify-center dark:bg-neutral-800 ">
-          <AiOutlineBell className="w-6  h-6" />
-        </button>
+        <IconButton>
+          <AiOutlineBell/>
+        </IconButton>
         <div className="flex flex-row gap-4">
           <div className="flex-col text-right hidden md:block lg:block">
             <h4 className="dark:text-white ">Nombre de usuario</h4>
