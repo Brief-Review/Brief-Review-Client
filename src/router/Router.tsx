e31f77f3ded74ae74c5171218facd9fab4154253
Gary HL <garyhl321@gmail.com>
2023-05-11 15:19:22 +0200

    add background in router

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,16 +1,18 @@
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes, HashRouter, BrowserRouter } from "react-router-dom";
 import Home from "../views/home/Home";
 import Messages from "../views/messages/Messages";
 
-function Router() {
+const Router = () => {
   return (
-    <HashRouter>
-      <>
+    <BrowserRouter>
+      <div className="bg-white dark:bg-black">
         <Routes>
-          <Route path="/" element={<Messages/>} />
+          <Route path="/" element={<Home/>} />
+          <Route path="/home" element={<Home/>} />
+          <Route path="/messages/:id" element={<Messages/>} />
         </Routes>
-      </>
-    </HashRouter>
+      </div>
+    </BrowserRouter>
   );
 }
 
