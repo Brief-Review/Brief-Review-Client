306cc8d89dac0de7a9f0eeb6f6bdb2b5f73e9002
Gary HL <garyhl321@gmail.com>
2023-05-11 15:17:21 +0200

    create component message

---
+++ src/components/chat/Message.tsx
@@ -0,0 +1,19 @@
+export interface messageData {
+  id: number,
+  name: string,
+  message: string,
+}
+
+function Message(props: { messageData: messageData }) {
+  return (
+    <div
+      className={`p-2 bg-white shadow-md my-4 rounded max-w-[80%] dark:bg-neutral-900  ${
+        props.messageData?.name === "me" ? " self-end" : "self-start"
+      }`}
+    >
+      {props.messageData?.message}
+    </div>
+  );
+}
+
+export default Message;
