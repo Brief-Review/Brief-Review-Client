dd82f84cb0183108f0bad20720240a8480545fa5
Gary HL <garyhl321@gmail.com>
2023-05-18 23:21:07 +0200

    created AppContext

---
+++ src/models/auth/User.model.tsx
@@ -0,0 +1,12 @@
+export interface User {
+    id:number,
+    name:string,
+    last_name:string,
+    avatar:string,
+    role:number,
+    email:string,
+    password:string,
+    phone_number:number,
+    address:string,
+    rewards:number,
+}
