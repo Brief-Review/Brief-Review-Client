9aa0bd4311698ce5605d487031c15b9911a20ec4
Gary HL <garyhl321@gmail.com>
2023-05-29 22:27:29 +0200

    use axios interceptor

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Router from "./router/Router";
+import { AxiosInterceptor } from "./interceptors/axios.interceptor";
 
 function App() {
+  AxiosInterceptor();
   return (
     <>
       <Router />
