762a5c621d878d393261c80919ed902424f38b20
Gary HL <garyhl321@gmail.com>
2023-05-29 22:33:38 +0200

    created new route

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -5,23 +5,25 @@ import Resources from "../views/resources/Resources";
 import Brief from "../views/brief/Brief";
 import Login from "../views/auth/Login";
 import Register from "../views/auth/Register";
+import AdminDashboard from "../views/adminDashboard/AdminDashboard";
 
 const Router = () => {
   return (
     <HashRouter>
       <div className="bg-white dark:bg-black">
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/home" element={<Home/>} />
-          <Route path="/messages/:id" element={<Messages/>} />
-          <Route path="/resources" element={<Resources/>} />
-          <Route path="/brief/:id" element={<Brief/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/register" element={<Register/>}/>
-        </Routes> 
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/messages/:id" element={<Messages />} />
+          <Route path="/resources" element={<Resources />} />
+          <Route path="/brief/:id" element={<Brief />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Routes>
       </div>
     </HashRouter>
   );
-}
+};
 
 export default Router;
