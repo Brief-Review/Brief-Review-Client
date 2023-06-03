93a96eb27cf0fb3beec8ad6aec58ae714746b92f
Gary HL <garyhl321@gmail.com>
2023-06-03 10:27:57 +0200

    add custom hook useLocalstorage

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+export const useLocalStorage = (key:string, initialValue:any) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+
+  const setValue = (value:any) => {
+    try {
+      setStoredValue(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
