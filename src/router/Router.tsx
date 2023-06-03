d729091074fd16de148751fc4bb6fc419ccfaf5a
Gary HL <garyhl321@gmail.com>
2023-06-03 10:27:44 +0200

    add custom hook useLocalstorage

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,4 +1,4 @@
-import { Route, Routes, HashRouter } from "react-router-dom";
+import { Route, Routes, useNavigate, useLocation } from "react-router-dom";
 import Home from "../views/home/Home";
 import Messages from "../views/messages/Messages";
 import Resources from "../views/resources/Resources";
@@ -7,24 +7,36 @@ import Login from "../views/auth/Login";
 import Register from "../views/auth/Register";
 import AdminDashboard from "../views/adminDashboard/AdminDashboard";
 import Briefings from "../views/Briefings/Briefings";
+import TestView from "../views/testView/TestView";
+import { useContext, useEffect } from "react";
+import AppContext from "../context/global/AppContext";
 
 const Router = () => {
+  const { token } = useContext(AppContext);
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    if (!token && location.pathname !== "/register") {
+      navigate("/login");
+    }
+  }, [token, location.pathname, navigate]);
+
   return (
-    <HashRouter>
-      <div className="bg-white dark:bg-black">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/messages/:id" element={<Messages />} />
-          <Route path="/resources" element={<Resources />} />
-          <Route path="/briefings" element={<Briefings />} />
-          <Route path="/brief/:id" element={<Brief />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-      </div>
-    </HashRouter>
+    <div className="bg-white dark:bg-black">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/messages/:id" element={<Messages />} />
+        <Route path="/resources" element={<Resources />} />
+        <Route path="/briefings" element={<Briefings />} />
+        <Route path="/brief/:id" element={<Brief />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/admin" element={<AdminDashboard />} />
+        <Route path="test" element={<TestView />} />
+      </Routes>
+    </div>
   );
 };
 
