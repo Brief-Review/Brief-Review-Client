bfe48acf76fda2476ca6d2c70fda07b09446a675
Gary HL <garyhl321@gmail.com>
2023-06-05 23:38:27 +0200

    simplify axios interceptor

--- src/interceptors/axios.interceptor.tsx
+++ src/interceptors/axios.interceptor.tsx
@@ -5,17 +5,8 @@ import AppContext from "../context/global/AppContext";
 export const AxiosInterceptor = () => {
   const { token } = useContext(AppContext);
 
-  useEffect(() => {
-    const interceptor = axios.interceptors.request.use((config) => {
-      config.headers.Authorization = `Bearer ${token}`;
-      return config;
-    });
-
-    return () => {
-      axios.interceptors.request.eject(interceptor);
-    };
-  }, [token]);
-
-  return null; // Opcionalmente, puedes devolver algún contenido si lo necesitas
+  const interceptor = axios.interceptors.request.use((config) => {
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  });
 };
-
